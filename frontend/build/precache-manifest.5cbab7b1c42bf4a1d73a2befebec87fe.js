70db767caf96cab708806e32b2c48cc7209701c6
davisperezg <davisperezg@gmail.com>
2021-01-27 22:21:18 -0500

    impresion de pdf arreglado

---
+++ frontend/build/precache-manifest.5cbab7b1c42bf4a1d73a2befebec87fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ac739baf8bbd6b87906aa75ac00239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cd69d10c327476e1cd7",
+    "url": "/static/css/3.82bfcd99.chunk.css"
+  },
+  {
+    "revision": "b18388f78d7ea486a301",
+    "url": "/static/css/main.eaf93920.chunk.css"
+  },
+  {
+    "revision": "04e90d3a197a790f882f",
+    "url": "/static/js/2.62dcf24c.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/2.62dcf24c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd69d10c327476e1cd7",
+    "url": "/static/js/3.b268991a.chunk.js"
+  },
+  {
+    "revision": "c396e98a9ac714872d33cfe88596894b",
+    "url": "/static/js/3.b268991a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4248690106a86c94951e",
+    "url": "/static/js/4.de25df91.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.de25df91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed99d12977c3349cfd5",
+    "url": "/static/js/5.da69c7e3.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.da69c7e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18388f78d7ea486a301",
+    "url": "/static/js/main.a1b9a07f.chunk.js"
+  },
+  {
+    "revision": "70c048d66ecd311ce5c2",
+    "url": "/static/js/runtime-main.8386896c.js"
+  }
+]);
